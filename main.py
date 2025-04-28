d5fa61feb59348abd2c76c2f71b9038132d919be
lmacias <luis.macias@sistemabea.mx>
2025-04-28 15:28:23 -0600

    libre

--- main.py
+++ main.py
@@ -1,12 +1,9 @@
 from common import *
 from sql.db import db_create
 from Menu.Menu import show_main_menu
-from tkinter import Tk, Toplevel
-import sys
 from monitor_exe import ejecutar_monitor
-from common import *
-from Menu.Menu import show_main_menu
 import locale
+
 
 def main():
     verificar_imports()
@@ -16,12 +13,15 @@ def main():
         locale.setlocale(locale.LC_ALL, 'en_US.UTF-8')
     except:
         locale.setlocale(locale.LC_ALL, '')
-    # Crear/verificar la base de datos
 
+    # Crear/verificar la base de datos
     db_path = db_create()
     print(db_path)
 
-    # Mostrar el menú principal
+
+    # Mostrar el menú principal inicialmente
     show_main_menu(db_path)
+
+
 if __name__ == "__main__":
     main()
